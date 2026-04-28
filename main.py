4f8982126b5ed1d0980d5544c9220174f299ee1e
kazuki <kazukishiroma06@gmail.com>
2026-04-28 13:03:44 -0300

    população o db

--- main.py
+++ main.py
@@ -12,20 +12,64 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 def exibir_home(request: Request, db:Session = Depends(get_db)):
     lista_produtos = db.query(Produto).all()
-    return templates.TemplateResponse(request, "index.html", {"request": request, "produtos": lista_produtos})
+    categorias_do_banco = db.query(Categoria).all()
+    return templates.TemplateResponse(request, "index.html", {"request": request, "produtos": lista_produtos, "categorias": categorias_do_banco})
 
 @app.get("/produtos/cadastro", response_class=HTMLResponse)
-def exibir_produtos(request: Request):
-    return templates.TemplateResponse(request, "cadastro_produto.html", {"request": request})
+def exibir_produtos(request: Request, db: Session = Depends(get_db)):
+    categorias = db.query(Categoria).all()
+    return templates.TemplateResponse("cadastro_produto.html", {"request": request, "categorias": categorias})
 
 @app.post("/produtos/salvar")
 def criar_produto(
     nome: str = Form(...),
     marca: str = Form(...),
+    categoria_id: int = Form(...),
     db: Session = Depends(get_db)
 ):
-    novo_produto = Produto(nome=nome, marca=marca)
+    novo_produto = Produto(nome=nome, marca=marca, categoria_id=categoria_id)
     db.add(novo_produto)
     db.commit()
 
     return RedirectResponse(url="/", status_code=303)
+
+@app.post("/produtos/deletar/{id}")
+def deletar_produtos(
+    id:int,
+    db: Session = Depends(get_db)
+):
+    produto = db.query(Produto).get(id)
+    if produto:
+        db.delete(id)
+        db.commit()
+
+    return RedirectResponse(url="/", status_code=303)
+
+@app.get("/categorias")
+def exibir_categorias(request: Request, db: Session = Depends(get_db)):
+    lista_categorias = db.query(Categoria).all()
+    return templates.TemplateResponse(request, "categorias.html", {"request": request, "categorias": lista_categorias})
+
+@app.post("/categorias/salvar")
+def criar_categorias(
+    nome: str = Form(...),
+    descricao: str = Form(...),
+    db: Session = Depends(get_db)
+):
+    nova_categoria = Categoria(nome=nome, descricao=descricao)
+    db.add(nova_categoria)
+    db.commit()
+
+    return RecursionError(url="/", status_code=303)
+
+@app.post("/categorias/deletar/{id}")
+def excluir_categoria(
+    id:int,
+    db: Session = Depends(get_db)
+):
+    categoria = db.query(Categoria).get(id)
+    if categoria:
+        db.delete(categoria)
+        db.commit()
+
+    return RedirectResponse(url="/categorias", status_code=303)
