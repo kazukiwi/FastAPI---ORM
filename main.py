58ec51f8cb3a2794f873a9998c82d5c779392994
kazuki <kazukishiroma06@gmail.com>
2026-04-28 10:00:50 -0300

    cadastro do produto e html

--- main.py
+++ main.py
@@ -14,3 +14,18 @@ def exibir_home(request: Request, db:Session = Depends(get_db)):
     lista_produtos = db.query(Produto).all()
     return templates.TemplateResponse(request, "index.html", {"request": request, "produtos": lista_produtos})
 
+@app.get("/produtos/cadastro", response_class=HTMLResponse)
+def exibir_produtos(request: Request):
+    return templates.TemplateResponse(request, "cadastro_produto.html", {"request": request})
+
+@app.post("/produtos/salvar")
+def criar_produto(
+    nome: str = Form(...),
+    marca: str = Form(...),
+    db: Session = Depends(get_db)
+):
+    novo_produto = Produto(nome=nome, marca=marca)
+    db.add(novo_produto)
+    db.commit()
+
+    return RedirectResponse(url="/", status_code=303)
